7165c0952cd959ec5175ada5bd7d877bc33e3711
Praveen07-bounteous <praveen.patil@bounteous.com>
2026-02-18 02:58:12 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length, i = 0;
+        while(i < n){
+            int correct = nums[i]-1;
+            if((correct >= 0 && correct < n) && nums[i] != nums[correct]) swap(nums, i, correct);
+            else i++;
+        }
+        for(int j=0; j<n; j++){
+            if(nums[j] != j+1) return j+1;
+        }
+        return n;
+    }
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
